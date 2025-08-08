8bc7ba0ba83eed8342c5eba9c724541b46af9b00
Euzebio-senai <matheus.e.ferreira@ba.estudante.senai.br>
2025-08-08 17:40:32 -0300

    Add files via upload

---
+++ easystock-completo/backend/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/produtos', require('./routes/produtos'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
